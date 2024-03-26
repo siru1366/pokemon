b4269e664bd258fa52bacad430dd6283df6857c8
siru1366 <150617036+siru1366@users.noreply.github.com>
2024-03-26 18:47:03 -0400

    Update 03-test_data.R

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,8 +1,8 @@
 #### Preamble ####
 # Purpose: Tests... [...UPDATE THIS...]
-# Author: Yunzhao Li [...UPDATE THIS...]
+# Author: Sirui Tan [...UPDATE THIS...]
 # Date: 26 March 2023 [...UPDATE THIS...]
-# Contact: yunzhao.li@mail.utoronto.ca [...UPDATE THIS...]
+# Contact:sirui.tan@mail.utoronto.ca [...UPDATE THIS...]
 # License: MIT
 # Pre-requisites: [...UPDATE THIS...]
 # Any other information needed? [...UPDATE THIS...]
